032d82ce69b11b3fb7b08e8a1351223d64ab6c3c
Hugo Hirsch <github@hugo-hirsch.de>
2023-07-18 23:01:29 +0200

    Add i18n

---
+++ sb2/src/main/java/de/aikiit/prototype/configuration/AllowValidationMessagesLocalized.java
@@ -0,0 +1,28 @@
+package de.aikiit.prototype.configuration;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+@Configuration
+public class AllowValidationMessagesLocalized {
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:sb-filter-problem");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    // inject our own message source into the validation factory of Hibernate
+    @Bean
+    public LocalValidatorFactoryBean validator(MessageSource messageSource) {
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource);
+        return bean;
+    }
+
+}
