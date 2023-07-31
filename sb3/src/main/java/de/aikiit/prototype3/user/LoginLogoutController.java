69646ce2055fcc78445ea71b0a4c314ed590ae05
Hugo Hirsch <github@hugo-hirsch.de>
2023-07-31 16:22:26 +0200

    Explicitly accept only GET for /login

--- sb3/src/main/java/de/aikiit/prototype3/user/LoginLogoutController.java
+++ sb3/src/main/java/de/aikiit/prototype3/user/LoginLogoutController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -15,7 +16,7 @@ public class LoginLogoutController {
         return new ModelAndView("/app/index.html");
     }
 
-    @RequestMapping("/login")
+    @GetMapping("/login")
     public ModelAndView login() {
         return new ModelAndView("login.html");
     }
