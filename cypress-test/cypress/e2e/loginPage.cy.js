801f4d42d52b1d0e659c225af3461c9da541a9f3
Hugo Hirsch <github@hugo-hirsch.de>
2023-10-25 18:43:35 +0200

    Perform login in test

--- cypress-test/cypress/e2e/loginPage.cy.js
+++ cypress-test/cypress/e2e/loginPage.cy.js
@@ -1,5 +1,13 @@
 describe('Verify login page is loading', () => {
   it('Visit login page', () => {
     cy.visit('http://localhost:8080/')
+
+    cy.get('input[id="organisation"]').type('A').should('have.value', 'A').click()
+    cy.get('input[id="username"]').type('auser').should('have.value', 'auser').click()
+    cy.get('input[id="password"]').type('auser').should('have.value', 'auser').click()
+
+    cy.get('#login').click();
+
+    
   })
 })
