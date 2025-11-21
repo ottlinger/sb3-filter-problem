336508c41372cced2faba8c3f0c63da3349da1d1
Hugo Hirsch <github@hugo-hirsch.de>
2025-11-21 15:02:10 +0100

    Migrate SB2 to contain a test that loads the class

---
+++ sb2/src/test/java/de/aikiit/prototype/PortalAppSB2Test.java
@@ -0,0 +1,11 @@
+package de.aikiit.prototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalAppSB2Test {
+    @Test
+    void contextLoads() {
+    }
+}
