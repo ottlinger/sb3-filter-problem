f29ae8ae4ff213be45f0af004b2e11e2c8791e17
Hugo Hirsch <github@hugo-hirsch.de>
2026-04-20 00:20:31 +0200

    #332: Adapt logging and do not extract tenant

--- sb4/src/main/java/de/aikiit/prototype4/seeding/UserSeedDataCreator.java
+++ sb4/src/main/java/de/aikiit/prototype4/seeding/UserSeedDataCreator.java
@@ -71,8 +71,8 @@ public class UserSeedDataCreator {
             Tenant t = targetTenant.get();
             User u;
             for (String username : USERS) {
-                log.info("Creating user {} in tenant {}", username, t);
-                u = User.builder().organisation(targetTenant.get()).fullName("Fullname " + username).userName(username).passwordHash(encryptPassword(username)).roles(Arrays.asList(user, admin)).build();
+                log.info("Creating user {} in tenant {}", username, t.getName());
+                u = User.builder().organisation(t).fullName("Fullname " + username).userName(username).passwordHash(encryptPassword(username)).roles(Arrays.asList(user, admin)).build();
                 log.info("User: {}", userRepository.save(u));
                 log.info("Tenant {} supplied with bootstrapped user {}.", t.getName(), username);
             }
