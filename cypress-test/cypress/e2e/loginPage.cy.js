77293e8b148b0cf197eb323dd38a91d40eb26d2f
Hugo Hirsch <github@hugo-hirsch.de>
2026-05-23 23:15:27 +0200

    #332: Fix IDE warnings by separating the calls

--- cypress-test/cypress/e2e/loginPage.cy.js
+++ cypress-test/cypress/e2e/loginPage.cy.js
@@ -1,14 +1,22 @@
-describe('Verify app is working completely', () => {
-  it('Visit login page, login and logout again', () => {
-    cy.visit('http://localhost:8080/')
+describe('Verify app is still working completely', () => {
+    it('Visit login page, login and logout again', () => {
+        cy.visit('http://localhost:8080/')
 
-    cy.get('input[id="organisation"]').type('A').should('have.value', 'A').click()
-    cy.get('input[id="username"]').type('auser').should('have.value', 'auser').click()
-    cy.get('input[id="password"]').type('auser').should('have.value', 'auser').click()
+        cy.get('input[id="organisation"]').type('A')
+        cy.get('input[id="organisation"]').click()
+        cy.get('input[id="organisation"]').should('have.value', 'A')
 
-    cy.get('#login').click();
-    cy.get('#logout').should('exist');
+        cy.get('input[id="username"]').type('auser')
+        cy.get('input[id="username"]').click()
+        cy.get('input[id="username"]').should('have.value', 'auser')
 
-    cy.get('#logout').click()
-  })
+        cy.get('input[id="password"]').type('auser')
+        cy.get('input[id="password"]').click()
+        cy.get('input[id="password"]').should('have.value', 'auser')
+
+        cy.get('#login').click()
+        cy.get('#logout').should('exist')
+
+        cy.get('#logout').click()
+    })
 })
